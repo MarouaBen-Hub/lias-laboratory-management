745d8269d077fb8791fc72cec44910e2c6bda21a
MarouaBen-Hub <maroua.benmijou@gmail.com>
2026-06-13 01:29:35 +0100

    Update ProcesVerbal.java

--- src/main/java/com/lias/lab/entity/ProcesVerbal.java
+++ src/main/java/com/lias/lab/entity/ProcesVerbal.java
@@ -3,23 +3,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.persistence.*;
-import lombok.*;
 
-import java.time.LocalDate;
-@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Entity
-@Table(name = "proces_verbal")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+public class Publication {
+}
 public class ProcesVerbal {
 
     @Id
